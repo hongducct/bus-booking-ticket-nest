d16abf1113bdab67f4680288eff7baca2efd19c9
hongducct <duchongkhoan100@gmail.com>
2025-11-07 17:45:13 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174', 'http://localhost:3001'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174', 'http://localhost:3001', 'https://bus.hongducct.id.vn'],
     credentials: true,
   });
 
